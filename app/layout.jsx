f9ef24b410fec9b6201375c6bbdce3b4e944c662
sanjai <rohithempire009@gmail.com>
2025-10-06 11:37:26 +0530

    meta tag change

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,12 +3,10 @@ import "./globals.css"
 import Header from "./sections/Components/Header"
 import SmoothScrollProvider from "@/components/SmoothScrollProvider"
 import Footer from "./sections/Footer"
-
 export const metadata = {
   title: "Webnox Digital | Software Development Company, Website & App Development Experts",
   description:
     "Webnox Digital is a leading software development company offering website development, AI software solutions, mobile app development, and digital marketing services. We help businesses innovate, scale, and succeed online.",
-
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: '32x32', type: 'image/ico' },
@@ -18,16 +16,17 @@ export const metadata = {
     apple: '/favicon.ico',
   },
 }
-
 const urbanist = Urbanist({
   variable: "--font-urbanist",
   subsets: ["latin"],
   display: "swap",
 })
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="dPoW0wzIi2VpSOg4ew-Ov7FLsv21XKLexjM8WpR6nlA" />
+      </head>
       <body className={`${urbanist.variable} font-sans antialiased`}>
         <Header />
         <SmoothScrollProvider>
